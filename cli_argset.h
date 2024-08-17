1440f23fd8317e72df799c84b456c04402b5b449
Anthony Ngo <anthonyngo002@gmail.com>
2024-08-17 00:58:41 -0700

    feat: autodetect filetype and import

---
+++ cli_argset.h
@@ -0,0 +1,12 @@
+#ifndef CLI_ARGSET
+#define CLI_ARGSET
+
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cstddef>
+#include <cmath>
+#include "harmonize.git/harmonize/cpp/util/cli.h"
+using namespace util;
+
+#endif
