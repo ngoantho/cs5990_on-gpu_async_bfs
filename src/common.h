f23b353f675c2ebaf8f7338b5e84161502dc9c9f
Anthony Ngo <anthonyngo002@gmail.com>
2024-08-22 15:16:17 -0700

    code style fix

--- src/common.h
+++ src/common.h
@@ -7,27 +7,27 @@
 #include "node.h"
 #include "cli_argset.h"
 
+void common_for(std::ostream& dest, std::vector<Node>& nodes) {
+  for (Node &i : nodes) {
+    dest << i.id << ": depth=" << i.depth << ", visited=" << i.visited << std::endl;
+  }
+}
+
 void common_output(cli::ArgSet& args, float runtime, std::vector<Node>& nodes, std::string type) {
   const char* output = args.get_flag_str((char*)"output");
   if (output == nullptr) {
     output = std::string("runtime").c_str();
   }
 
-  auto common_for = [nodes](std::ostream& dest) {
-    for (auto &&i : nodes) {
-      dest << i.id << ": depth=" << i.depth << ", visited=" << i.visited << std::endl;
-    }
-  };
-
   if (std::string(output) == "runtime") {
     std::cout << "Runtime: " << runtime << "ms" << std::endl;
   } else if (std::string(output) == "state") {
-    common_for(std::cout);
+    common_for(std::cout, nodes);
   } else if (std::string(output) == "write") {
-    std::string filename = "/tmp/"+type+".txt";
+    std::string filename = "./out/"+type+".txt";
     std::cout << "writing to: " << filename << std::endl;
     std::ofstream file(filename);
-    common_for(file); // ios_base <- ios <- ostream <- ofstream
+    common_for(file, nodes); // ios_base -> ios -> ostream -> ofstream
     file.close();
   } else {
     std::cerr << "unknown output value: " << std::string(output) << std::endl;
