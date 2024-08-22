9f9f0b3df5f722727cb45c83ee571a95b6781fd8
Anthony Ngo <anthonyngo002@gmail.com>
2024-08-22 13:10:37 -0700

    main file parser: print different heuristics

--- src/main_file_parser.cpp
+++ src/main_file_parser.cpp
@@ -1,13 +1,13 @@
-#include "file_parser.h"
 #include "cli_argset.h"
-#include <sstream>
+#include "file_parser.h"
 #include <iterator>
+#include <sstream>
 #include <vector>
 
-std::string vec_to_string(std::vector<int>& vec) {
+std::string vec_to_string(std::vector<int> &vec) {
   if (vec.size() > 0) {
     std::ostringstream oss;
-    std::copy(vec.begin(), vec.end()-1, std::ostream_iterator<int>(oss, " "));
+    std::copy(vec.begin(), vec.end() - 1, std::ostream_iterator<int>(oss, " "));
     oss << vec.back();
     return oss.str();
   } else {
@@ -15,20 +15,28 @@ std::string vec_to_string(std::vector<int>& vec) {
   }
 }
 
-int main(int argc, char* argv[]) {
+int max_key(std::map<int, std::vector<int>>& graph) {
+  std::vector<int> vec;
+  for (auto &&pair : graph)
+  {
+    vec.push_back(pair.first);
+  }
+  return *std::max_element(vec.begin(), vec.end());
+}
+
+int main(int argc, char *argv[]) {
   cli::ArgSet args(argc, argv);
   bool directed = args["directed"];
-  char* file_str = args.get_flag_str((char*)"file");
-  
+  char *file_str = args.get_flag_str((char *)"file");
+
   FileParser file_parser;
-  // if (file_parser.known_extension(file_str)) file_parser.parse_extension(file_str);
-  // else file_parser.parse_arguments(args);
+  // if (file_parser.known_extension(file_str))
+  // file_parser.parse_extension(file_str); else
+  // file_parser.parse_arguments(args);
   file_parser.parse_arguments(args);
 
-  std::map<int, std::vector<int>> graph = file_parser.parse_file(file_str, directed, true);
-  std::cout << "graph size: " << graph.size() << std::endl;
+  std::map<int, std::vector<int>>& graph = file_parser.parse_file(file_str, directed, true);
 
-  for (auto &&i : graph) {
-    std::cout << i.first << ": " << vec_to_string(i.second) << std::endl;
-  }
+  std::cout << "max node - graph.rbegin: " << graph.rbegin()->first << std::endl;
+  std::cout << "max node - max element: " << max_key(graph) << std::endl;
 }
