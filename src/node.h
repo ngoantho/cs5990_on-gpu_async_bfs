753b83b667758741cbfeb969e8a44f1413c37ca0
Anthony Ngo <anthonyngo002@gmail.com>
2024-08-24 00:25:23 -0700

    node: create using only id

--- src/node.h
+++ src/node.h
@@ -1,12 +1,16 @@
 #ifndef NODE
 #define NODE
 
-typedef struct {
+struct Node {
   int id;
   size_t edge_count;
   size_t edge_offset;
   unsigned int depth;
   unsigned int visited;
-} Node;
+
+  Node() {}
+
+  Node(int _id) : id(_id), edge_count(0), depth(0xFFFFFFFF), visited(0) {}
+};
 
 #endif
