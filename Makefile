389a7c1b2e2a99f3caf1a27c6eeb73e6231875b6
Anthony Ngo <anthonyngo002@gmail.com>
2024-08-28 11:42:47 -0700

    create main_node_graph

--- Makefile
+++ Makefile
@@ -12,5 +12,8 @@ harmonize: src/main_harmonize.cu
 file_parser: src/file_parser.h src/main_file_parser.cpp
 	g++ -g -std=c++17 -o file_parser.exe src/main_file_parser.cpp
 
+node_graph: src/node_graph.h src/main_node_graph.cpp
+	g++ -g -std=c++17 -o node_graph.exe src/main_node_graph.cpp
+
 main: src/main_queue.cpp src/main_harmonize.cu src/main.cpp
 	nvcc -g -G -x cu -std=c++17 -o main.exe src/main.cpp
---
+++ src/main_node_graph.cpp
@@ -0,0 +1,29 @@
+#include "cli_argset.h"
+#include "file_parser.h"
+#include "node_graph.h"
+
+int main(int argc, char *argv[]) {
+  cli::ArgSet args(argc, argv);
+  bool directed = args["directed"];
+  char *file_str = args.get_flag_str((char *)"file");
+
+  FileParser file_parser;
+  if (file_parser.known_extension(file_str)) 
+    file_parser.parse_extension(file_str); 
+  else 
+    file_parser.parse_arguments(args);
+
+  std::map<int, std::vector<int>>& adjgraph = file_parser.parse_file(file_str, directed);
+  NodeGraph node_graph(adjgraph);
+  
+  for (Node& node : node_graph.nodes) {
+    std::cout << node.id << ": edge_count=" << node.edge_count << std::endl;
+    size_t offset = node.edge_offset;
+    for (size_t i = 0; i < node.edge_count; i++)
+    {
+      int edge_node_id = node_graph.edges[offset + i];
+      std::cout << "- " << edge_node_id << std::endl;
+    }
+    std::cout << std::endl;
+  }
+}
