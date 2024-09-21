216e73d6609bd1755b5445dde0658647f01a1f5b
Anthony Ngo <anthonyngo002@gmail.com>
2024-09-22 03:16:36 +0700

    move root node generation to separate func

--- script_common.py
+++ script_common.py
@@ -27,6 +27,23 @@ datasets = """
 def subprocess_call(process:str, dataset:str, root_node:str, program:str|None, directed:bool, *args):
   return subprocess.run([process, '-file', dataset, '-root', root_node, "-program", program if program != None else str(None), "-directed" if directed else "", *args], stdout=subprocess.PIPE)
 
+def get_root_node(dataset:str):
+  nodes = []
+  with open(dataset, 'r') as file:
+    delimiter = None
+    if dataset.endswith("mtx"): delimiter = " "
+    elif dataset.endswith("csv"): delimiter = ","
+    else: raise Exception("unsupported file format")
+
+    for line in file.readlines():
+        splitted = line.split(delimiter)
+        try:
+            node, edge = int(splitted[0]), int(splitted[1])
+            nodes.append(node)
+        except Exception as e:
+            pass
+  return random.choice(nodes)
+
 def run(output_filename:str, process:str, program:str, directed:bool, *args:list[str]):
   output = open(output_filename, "w")
   output.write("dataset,root node,program,directed,runtime (ms)\n")
@@ -34,23 +51,8 @@ def run(output_filename:str, process:str, program:str, directed:bool, *args:list
   for dataset in datasets.splitlines():
     if dataset == "": continue
     elif "#" in dataset: continue
-
-    nodes = []
-    with open(dataset, 'r') as file:
-        delimiter = None
-        if dataset.endswith("mtx"): delimiter = " "
-        elif dataset.endswith("csv"): delimiter = ","
-        else: raise Exception("unsupported file format")
-
-        for line in file.readlines():
-            splitted = line.split(delimiter)
-            try:
-                node, edge = int(splitted[0]), int(splitted[1])
-                nodes.append(node)
-            except Exception as e:
-                pass
     
-    root_node = str(random.choice(nodes))
+    root_node = str(get_root_node(dataset))
     dataset_filename = path.basename(dataset)
 
     call = subprocess_call(process, dataset, root_node, program, directed, *args)
